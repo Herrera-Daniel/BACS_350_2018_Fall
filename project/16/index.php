34462e4075c823717ca13e31e632efdf9c862c3e
Daniel Herrera <dan_michigan2012@hotmail.com>
2018-09-28 15:08:56 -0600

    16.2 - Create app page

---
+++ project/16/index.php
@@ -0,0 +1,18 @@
+ <?php
+
+    // Start the page
+    $page_title = 'BACS 350 - Project #16.2 - Page Pattern';
+    require_once 'views.php';
+    begin_page($page_title);
+
+    echo '
+        <p>This is a sample page built with functions
+
+    ';
+    
+
+
+    // End the page
+    end_page();
+
+?>
