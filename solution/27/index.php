0c6554e1097247121e5fbd1dde74243f759bea34
Daniel Herrera <dan_michigan2012@hotmail.com>
2018-10-26 14:22:49 -0600

    Update

--- solution/27/index.php
+++ solution/27/index.php
@@ -3,20 +3,21 @@
     require_once 'views.php';
 
 
-//    // Log the page load
-//    require_once 'log.php';
-//    $log->log_page("solution/27");
+//     Log the page load
+    require_once 'log.php';
+    //$log->log_page("solution/27");
+    echo '<a href="pagelog.php">Page Log</a>';
 
-//    if (! isset($_COOKIE['FIRST_TIME'])) {
-//        $_COOKIE['FIRST_TIME'] = "FALSE");
-//    } 
+    if (! isset($_COOKIE['FIRST_TIME'])) {
+        $_COOKIE['FIRST_TIME'] = "TRUE";
+    } 
 //        
 //
 //    if (! isset($_SESSION['SHOPPING'])) {
 //        $_SESSION['SHOPPING'] = 'FALSE';
 //    }
 //        
-//    $log->log_page( "solution/27  Cookie First: $_COOKIE[FIRST_TIME]");
+    $log->log_page( "solution/27  Cookie First: $_COOKIE[FIRST_TIME]");
 //    $log->log_page( "solution/27  Cookie Shopping: $_COOKIE[SHOPPING]");
 
 
@@ -30,7 +31,7 @@
     }
 
     function show_not_first_time() {
-//        setcookie ('FIRST_TIME', 'TRUE');
+        //setcookie ('FIRST_TIME', 'TRUE');
         return 'NOT First time';
         
     }
@@ -51,37 +52,37 @@
 
     $content = "<h1>Demo of Cookies and Sessions</h1>";
     
-//    if (! isset($_COOKIE['FIRST_TIME']) or $_COOKIE['FIRST_TIME'] == "TRUE") {
-//        $message = show_first_time();
-//    }
-//    else {
-//        $message = show_not_first_time();
-//    }
-//    $content .= render_card ("Welcome",$message);
+    if ($_COOKIE['FIRST_TIME'] == "TRUE") {
+        $message = show_first_time();
+    }
+    else {
+        $message = show_not_first_time();
+    }
+    $content .= render_card ("Welcome",$message);
 //
 //
-//    // Handle Shopping Cart
-//
-//    $shopping = filter_input(INPUT_GET, 'shopping');
-//
-//    if ($shopping == 'TRUE') {
-//        session_start ();
-//        $_SESSION['SHOPPING'] = 'TRUE';
-//    }
-//    else {
-//        $_SESSION['SHOPPING'] = 'FALSE';
-//    }
-//
-//    if (isset($_SESSION['SHOPPING']) and $_SESSION['SHOPPING']=='TRUE') {
-//        $message =  show_shopping_cart();
-//    }
-//    else {
-//        $message =  show_buy_now();
-//    }
-//    $content .= render_card ("PURCHASE", $message);
+    // Handle Shopping Cart
+
+    $shopping = filter_input(INPUT_GET, 'shopping');
+
+    if ($shopping == 'TRUE') {
+        session_start ();
+        $_SESSION['SHOPPING'] = 'TRUE';
+    }
+    else {
+      $_SESSION['SHOPPING'] = 'FALSE';
+    }
+
+    if (isset($_SESSION['SHOPPING']) and $_SESSION['SHOPPING']=='TRUE') {
+        $message =  show_shopping_cart();
+    }
+    else {
+        $message =  show_buy_now();
+    }
+   $content .= render_card ("PURCHASE", $message);
 //    
-//    $after = "After: Cookie = $_COOKIE[FIRST_TIME]";
-//    $status = render_card("Cookies", $before . $after);
+    $after = "After: Cookie = $_COOKIE[FIRST_TIME]";
+    $status = render_card("Cookies", $before . $after);
 
 
 
@@ -90,7 +91,7 @@
         "site_title" => "BACS 350 Projects",
         "page_title" => "Cookies & Sessions", 
         "style"      => 'style.css',
-        "content"    => $content);
+        "content"    => $content . $status);
 
     echo render_page($settings);
 
