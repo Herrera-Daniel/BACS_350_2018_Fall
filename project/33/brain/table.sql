3eaa3947dc676dd9ca93d04508c44069b0f6a380
Daniel Herrera <dan_michigan2012@hotmail.com>
2018-11-19 15:13:30 -0700

    update

--- project/33/brain/table.sql
+++ project/33/brain/table.sql
@@ -11,3 +11,13 @@ CREATE TABLE notes (
 );
 
 
+CREATE TABLE slides (
+    
+    id          INT             NOT NULL  AUTO_INCREMENT,
+    title       VARCHAR(255)    NOT NULL, 
+    author       VARCHAR(255),
+    date        VARCHAR(255),
+    body        TEXT,
+    PRIMARY KEY (id)
+    
+);
